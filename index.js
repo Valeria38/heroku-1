21feeb064812b218785d73ed39b2c151f572b956
Valeria Gora <valeriia308@gmail.com>
2019-04-13 17:50:46 +0300

    file changed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Homepage'));
+app.get('/', (req, res) => res.send('Homepage1'));
 
 app.listen(port);
